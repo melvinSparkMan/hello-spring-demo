ad0e7da110a1066ae2c50918847d09abc564b486
Melvin Sparkman <93449867+melvinSparkMan@users.noreply.github.com>
2022-02-09 16:21:51 -0600

    Controllers and Springs: Exercises

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,61 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by Chris Bay
+ */
+@Controller
+@ResponseBody
+@RequestMapping("hello")
+public class HelloController {
+
+    //    // Handle request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
+
+    // lives at /hello/goodbye
+    @GetMapping("goodbye")
+    public String goodbye() {
+        return "Goodbye, Spring!";
+    }
+
+    // Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    // Handles requests of the form /hello/LaunchCode
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    // /hello/form
+    @GetMapping("form")
+    public String helloForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action = '/hello' method = 'post'>" + // submit a request to /hello
+                "<select name=\"language\" id=\"language-select\">\n" +
+                "    <option value=\"\">--Please choose a Language--</option>\n" +
+                "    <option value=\"Hello\">English</option>\n" +
+                "    <option value=\"Bon Jour\">French</option>\n" +
+                "    <option value=\"Hallo\">German</option>\n" +
+                "    <option value=\"Hola\">Spanish</option>\n" +
+                "    <option value=\"こんにちは\">Japanese</option>\n" +
+                "</select>" +
+                "<input type = 'text' name = 'name' >" +
+                "<input type = 'submit' value = 'Greet Me!' >" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+}
+
